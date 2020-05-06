9902153ebb0046cd0eab6bc3ed66ac3b0f6e11f0
Maksim Kostromin <daggerok@gmail.com>
2020-05-07 01:24:16 +0300

    Version control is awful (C) whatthecommit.com

--- test/big-int.test.js
+++ test/big-int.test.js
@@ -1,11 +1,11 @@
-// import { describe, it, expect } from 'jest';
+// import { describe, it, expect } from 'jest'; // from '@jest/globals';
 describe('BigInt', () => {
   it('old number', () => {
     let oldMaxNumber = Number.MAX_SAFE_INTEGER; // <- 9007199254740992
     expect(++oldMaxNumber).toBe(9007199254740992);
     expect(++oldMaxNumber).toBe(9007199254740992);
   });
-  it('new number', () => {
+  it('should use new BigInt number', () => {
     let oldMaxNumber = 9007199254740992n;
     expect(++oldMaxNumber).toBe(9007199254740993n);
     expect(++oldMaxNumber).toBe(9007199254740994n);
